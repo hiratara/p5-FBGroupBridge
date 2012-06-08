f31dcada584a8d641df2c77a89fc20a26aa05aac
Masahiro Honma <hiratara@cpan.org>
2012-06-08 16:02:32 +0900

    initial commit

---
+++ lib/Hiratara/FBGroupBridge.pm
@@ -0,0 +1,32 @@
+package Hiratara::FBGroupBridge;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Hiratara::FBGroupBridge -
+
+=head1 SYNOPSIS
+
+  use Hiratara::FBGroupBridge;
+
+=head1 DESCRIPTION
+
+Hiratara::FBGroupBridge is
+
+=head1 AUTHOR
+
+hiratara E<lt>hiratara {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
